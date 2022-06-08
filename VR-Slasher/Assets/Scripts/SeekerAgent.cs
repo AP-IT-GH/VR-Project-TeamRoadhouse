a54eac50e38472a3d0faa2c4b3c978afaeb8c160
Jef <jef.v.d.a@live.be>
2022-06-08 23:13:19 +0200

    Fixed portal

--- VR-Slasher/Assets/Scripts/SeekerAgent.cs
+++ VR-Slasher/Assets/Scripts/SeekerAgent.cs
@@ -8,55 +8,56 @@ public class SeekerAgent : Agent
 {
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float rotationSpeed = 350;
-    [SerializeField] private MonitorTool monitorTool;
-    [SerializeField] private float maxTime = 60f;
+    // [SerializeField] private MonitorTool monitorTool;
+    // [SerializeField] private float maxTime = 60f;
+    [SerializeField] private Transform target;
     
     private Rigidbody rb;
-    private Environment env;
-    private float timer = 0f;
+    // private Environment env;
+    // private float timer = 0f;
 
     public override void Initialize()
     {
         rb = GetComponent<Rigidbody>();
-        env = GetComponentInParent<Environment>();
-        timer = maxTime; // Reset timer
+        //env = GetComponentInParent<Environment>();
+        //timer = maxTime; // Reset timer
     }
 
     private void Update()
     {
-        // If agent falls, give negative reward and end episode
-        if(transform.localPosition.y < -3f)
-        {
-            monitorTool.FailsCount += 1;
-            EndEpisode();
-        }
+        //// If agent falls, give negative reward and end episode
+        //if(transform.localPosition.y < -3f)
+        //{
+        //    monitorTool.FailsCount += 1;
+        //    EndEpisode();
+        //}
 
-        // Create timer to give the agent a maximum time to find the player
-        if(timer <= 0f)
-        {
-            monitorTool.FailsCount += 1;
-            EndEpisode();
-        }
-        timer -= Time.deltaTime; // Take time elapsed from timer
+        //// Create timer to give the agent a maximum time to find the player
+        //if(timer <= 0f)
+        //{
+        //    monitorTool.FailsCount += 1;
+        //    EndEpisode();
+        //}
+        //timer -= Time.deltaTime; // Take time elapsed from timer
     }
 
-    public override void OnEpisodeBegin()
-    {
-        monitorTool.EpisodesCount += 1;
+    //public override void OnEpisodeBegin()
+    //{
+    //    monitorTool.EpisodesCount += 1;
 
-        rb.angularVelocity = Vector3.zero;
-        rb.velocity = Vector3.zero;
+    //    rb.angularVelocity = Vector3.zero;
+    //    rb.velocity = Vector3.zero;
 
-        timer = maxTime;
+    //    timer = maxTime;
 
-        env.ResetEnvironment();
-    }
+    //    env.ResetEnvironment();
+    //}
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.position); // 3
         sensor.AddObservation(transform.rotation); // 4
-        sensor.AddObservation(env.players[0].transform.position); // 3
+        sensor.AddObservation(target.position); // 3
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -85,30 +86,30 @@ public class SeekerAgent : Agent
         }
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        // Stop Episode when Agent finds player - SET REWARD TO 10
-        if (collision.transform.CompareTag("Player"))
-        {
-            SetReward(1f);
-            monitorTool.SuccesCount += 1;
-            EndEpisode();
-        }
-    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    // Stop Episode when Agent finds player - SET REWARD TO 10
+    //    if (collision.transform.CompareTag("Player"))
+    //    {
+    //        SetReward(1f);
+    //        monitorTool.SuccesCount += 1;
+    //        EndEpisode();
+    //    }
+    //}
 
-    public override void Heuristic(in ActionBuffers actionBuffers)
-    {
-        base.Heuristic(actionBuffers);
+    //public override void Heuristic(in ActionBuffers actionBuffers)
+    //{
+    //    base.Heuristic(actionBuffers);
 
-        var outputAction = actionBuffers.DiscreteActions;
-        outputAction[0] = 0;
-        outputAction[1] = 0;
+    //    var outputAction = actionBuffers.DiscreteActions;
+    //    outputAction[0] = 0;
+    //    outputAction[1] = 0;
 
-        if (Input.GetKey(KeyCode.UpArrow)) // Moving forwards
-            outputAction[0] = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) // Turning left
-            outputAction[1] = 1;
-        else if (Input.GetKey(KeyCode.RightArrow)) // Turning right
-            outputAction[1] = 2;
-    }
+    //    if (Input.GetKey(KeyCode.UpArrow)) // Moving forwards
+    //        outputAction[0] = 1;
+    //    if (Input.GetKey(KeyCode.LeftArrow)) // Turning left
+    //        outputAction[1] = 1;
+    //    else if (Input.GetKey(KeyCode.RightArrow)) // Turning right
+    //        outputAction[1] = 2;
+    //}
 }
