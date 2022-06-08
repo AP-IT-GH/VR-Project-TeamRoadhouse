ffafce202cb53489fc42d9b46b49f98a5d37d364
Jef <jef.v.d.a@live.be>
2022-06-08 23:39:45 +0200

    Fixed enemy kill

--- VR-Slasher/Assets/Scripts/VRplayer.cs
+++ VR-Slasher/Assets/Scripts/VRplayer.cs
@@ -7,13 +7,11 @@ public class VRplayer : MonoBehaviour
     public NextLevel Nextlevel;
     void OnTriggerEnter(Collider other)
     {
+        print(other.gameObject.tag);
         // if player collides with obj and level is not null advance to nextLevel
-        if (other.gameObject.CompareTag("Portal"))
+        if (other.gameObject.CompareTag("Portal") || other.gameObject.CompareTag("Agent"))
         {
-            // check wich level has to be loaded
-
             Nextlevel.Load();
-
         }
     }
 }
